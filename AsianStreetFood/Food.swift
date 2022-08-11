a76862e6639112f9a7b35ee2c782f7b0b8c0e93e
Dusan-O <dusan.orescanin@gmail.com>
2022-08-11 02:19:43 +0200

    struct Food, [Food], IBAction, func getRoundedImages(), func setupViews(), func viewDidAppear(),

---
+++ AsianStreetFood/Food.swift
@@ -0,0 +1,15 @@
+//
+//  Food.swift
+//  AsianStreetFood
+//
+//  Created by Dusan Orescanin on 11/08/2022.
+//
+
+import Foundation
+
+struct Food {
+    var name: String
+    var desc: String
+    var country: String
+    var image: String
+}
